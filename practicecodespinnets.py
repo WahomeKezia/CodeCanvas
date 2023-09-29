107abca8f96dcc6e3a71faf4430ee94d409c534a
KeziaWahome <k.wahome@alustudent.com>
2023-09-29 11:52:48 +0200

    changes

---
+++ practicecodespinnets.py
@@ -0,0 +1,17 @@
+# Testing is playwrite in stalled in my computer 
+#this code
+
+from playwright.sync_api import sync_playwright
+target_url = "https://www.example.com/"
+element = "h1"
+with sync_playwright() as playwright:
+    webkit = playwright.webkit
+    browser = webkit.launch()
+    browser_context = browser.new_context()
+    page = browser_context.new_page()
+    page.goto(target_url)
+    found_element = page.locator(element)
+    text = found_element.text_content()
+    browser.close()
+
+text
