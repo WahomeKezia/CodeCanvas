d489dc802207977fcc2bad2bb792a7dbe953ae75
KeziaWahome <k.wahome@alustudent.com>
2023-11-04 15:38:01 +0200

    fix:resolved internal server error

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# # Enable debug mode
+# app.debug = True
+
+# @app.route("/", methods=["GET"])
+# def code():
+#     return "Hello Flask app"
+
+# if __name__ == '__main__':
+#     # Run the app with the debugger enabled
+#     app.run(host='0.0.0.0', port=5000)
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Enable debug mode
-app.debug = True
-
 @app.route("/", methods=["GET"])
-def code():
-    return " Hello , Flask"
+def code_input():
+    return render_template("code_input.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
